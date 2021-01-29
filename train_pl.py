19f9f71a5f215e3de7cd0420d59dd8bbf02b7b92
[Duc Nguyen] <[duyduc1110@gmail.com]>
2021-01-29 10:21:20 +0700

    match Pytorch-Lightning 1.1.6

--- train_pl.py
+++ train_pl.py
@@ -2,9 +2,11 @@ import pytorch_lightning as pl
 import torch
 import argparse
 import logging
+import transformers
 
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.utilities import rank_zero_only
 from bonz_model import BonzConfig, BonzLM, BonzClassification, BonzModelGAN
 from transformers import BertTokenizerFast
 from bonz_model.utils import BonzDataset, BonzDataCollar
@@ -12,45 +14,6 @@ from datasets import load_from_disk, load_dataset, concatenate_datasets
 from torch.cuda.amp import autocast
 
 torch.random.manual_seed(42)
-
-
-'''
-class CheckpointEveryNSteps(pl.Callback):
-    """
-    Save a checkpoint every N steps, instead of Lightning's default that checkpoints
-    based on validation loss.
-    """
-
-    def __init__(
-        self,
-        save_step_frequency,
-        prefix="N-Step-Checkpoint",
-        use_modelcheckpoint_filename=True,
-    ):
-        """
-        Args:
-            save_step_frequency: how often to save in steps
-            prefix: add a prefix to the name, only used if
-                use_modelcheckpoint_filename=False
-            use_modelcheckpoint_filename: just use the ModelCheckpoint callback's
-                default filename, don't use ours.
-        """
-        self.save_step_frequency = save_step_frequency
-        self.prefix = prefix
-        self.use_modelcheckpoint_filename = use_modelcheckpoint_filename
-
-    def on_batch_end(self, trainer: pl.Trainer, _):
-        """ Check if we should save a checkpoint after every train batch """
-        epoch = trainer.current_epoch
-        global_step = trainer.global_step
-        if global_step % self.save_step_frequency == 0:
-            if self.use_modelcheckpoint_filename:
-                filename = trainer.checkpoint_callback.filename
-            else:
-                filename = f"{self.prefix}_{epoch=}_{global_step=}.ckpt"
-            ckpt_path = os.path.join(trainer.checkpoint_callback.dirpath, filename)
-            trainer.save_checkpoint(ckpt_path)
-'''
 
 
 class PrintOutModelCB(pl.Callback):
@@ -95,20 +58,26 @@ class BonzLM_PL(pl.LightningModule):
 
         self.save_hyperparameters(config)
         # Log metrics
-        self.total_loss_per_log = 0
-        self.gen_loss_per_log = 0
-        self.dis_loss_per_log = 0
+        self.total_loss_per_log = []
+        self.gen_loss_per_log = []
+        self.dis_loss_per_log = []
+
+    def log_metrics(self):
+        num_gpu = self.trainer.num_gpus
+        log_step = self.trainer.log_every_n_steps
+        f = lambda x: torch.tensor(x[-log_step*num_gpu:]).sum()/log_step
+        metrics = {
+            'num_samples': (self.global_step + 1) * self.config['batch_size'] * 3,
+            'train/total_loss': f(self.total_loss_per_log),
+            'train/gen_loss': f(self.gen_loss_per_log),
+            'train/dis_loss': f(self.dis_loss_per_log),
+        }
+        self.log_dict(metrics, on_step=True, on_epoch=False)
 
     def loss_accumulation(self, gen_loss, dis_loss):
-        self.total_loss_per_log += (gen_loss + dis_loss)
-        self.gen_loss_per_log += gen_loss
-        self.dis_loss_per_log += dis_loss
-
-    def should_reset_loss(self):
-        if (self.global_step + 1) % self.trainer.log_every_n_steps == 0:
-            self.total_loss_per_log = 0
-            self.gen_loss_per_log = 0
-            self.dis_loss_per_log = 0
+        self.total_loss_per_log.append(gen_loss + dis_loss)
+        self.gen_loss_per_log.append(gen_loss)
+        self.dis_loss_per_log.append(dis_loss)
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         with autocast():
@@ -126,52 +95,36 @@ class BonzLM_PL(pl.LightningModule):
         self.manual_backward(dis_loss, dis_opt)
         dis_opt.step()
 
-        # Logging when training
-        self.log('num_samples', (self.global_step + 1) * batch['input_ids'].shape[0] * 3, logger=True, on_step=True)
-
-        # Store losses
+        # Accumulate losses
         self.loss_accumulation(gen_loss.cpu().item(), dis_loss.cpu().item())
 
-        # Logging all losses
-        total_loss = (gen_loss + dis_loss).cpu().item()
-        self.log('train/total_loss', self.total_loss_per_log/self.trainer.log_every_n_steps, logger=True, on_step=True, on_epoch=False)
-        self.log('train/gen_loss', self.gen_loss_per_log/self.trainer.log_every_n_steps, logger=True, on_step=True, on_epoch=False)
-        self.log('train/dis_loss', self.dis_loss_per_log/self.trainer.log_every_n_steps, logger=True, on_step=True, on_epoch=False)
-        self.should_reset_loss()
+        # Log all losses
+        self.log_metrics()
 
         # Logging total loss to the progress bar
-        self.log('l', total_loss, prog_bar=True, logger=False, on_step=True)
+        self.log('total_loss',
+                 sum(self.total_loss_per_log[-self.trainer.num_gpus:]),
+                 prog_bar=True,
+                 logger=False,
+                 on_step=True,
+                 on_epoch=False)
 
     def configure_optimizers(self):
         gen_optimizer = torch.optim.AdamW(self.model.generator.parameters(), lr=self.config['lr'], weight_decay=0.001)
         dis_optimizer = torch.optim.AdamW(self.model.discriminator.parameters(), lr=self.config['lr'], weight_decay=0.001)
 
         gen_lr_scheduler = {
-            'scheduler': torch.optim.lr_scheduler.OneCycleLR(gen_optimizer,
-                                                             self.config['lr'],
-                                                             total_steps=self.config['training_step'],
-                                                             pct_start=0.05,
-                                                             anneal_strategy='cos',
-                                                             cycle_momentum=True,
-                                                             base_momentum=0.85,
-                                                             max_momentum=0.95,
-                                                             div_factor=10.0,
-                                                             final_div_factor=20.0),
+            'scheduler': transformers.get_linear_schedule_with_warmup(gen_optimizer,
+                                                                      0.04 * self.config['training_step'],
+                                                                      self.config['training_step']),
             'name': 'train/learning_rate',
             'interval': 'step',
             'frequency': 1}
 
         dis_lr_scheduler = {
-            'scheduler': torch.optim.lr_scheduler.OneCycleLR(dis_optimizer,
-                                                             self.config['lr'],
-                                                             total_steps=self.config['training_step'],
-                                                             pct_start=0.05,
-                                                             anneal_strategy='cos',
-                                                             cycle_momentum=True,
-                                                             base_momentum=0.85,
-                                                             max_momentum=0.95,
-                                                             div_factor=10.0,
-                                                             final_div_factor=20.0),
+            'scheduler': transformers.get_linear_schedule_with_warmup(dis_optimizer,
+                                                                      0.04 * self.config['training_step'],
+                                                                      self.config['training_step']),
             'name': 'train/learning_rate',
             'interval': 'step',
             'frequency': 1}
@@ -198,7 +151,7 @@ def get_args():
     # Trainer arguments
     model_parser.add_argument('--lr', default=1e-4, type=float, help='Learning rate')
     model_parser.add_argument('--training_step', default=100000, type=int, help='Training steps')
-    model_parser.add_argument('--batch_size', default=64, type=int, help='Batch size')
+    model_parser.add_argument('--batch_size', default=57, type=int, help='Batch size per device')
     model_parser.add_argument('--log_step', default=500, type=int, help='Steps per log')
 
     # Add BonzConfig parameter
